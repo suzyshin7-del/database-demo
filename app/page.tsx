3b7e61956f0882576bdec4561178f4f0e55c2eac
Suji shin <101472448@georgebrown.ca>
2026-03-18 18:53:03 +0000

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,6 @@ export default async function Home() {
   let { data: projects, error } = await supabase
     .from('projects')
     .select('*')
-    console.log("projects:", projects)
-console.log("error:", error)
-
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
